43be6d0c37328d86e308078c01e166f687df667f
attrix <lucho.bernal@hotmail.com>
2021-11-03 17:12:09 -0300

    view add device

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddDeviceComponent } from './pages/add-device/add-device.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DashboardComponent,
+  },
+  {
+    path: 'addDevice',
+    component: AddDeviceComponent,
   },
 ];
 
